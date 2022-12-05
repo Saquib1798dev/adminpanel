95efa59c3a5dbdd1898e1bf5b6709ddf6ed377de
Saquib <saquib.qureshi@protonshub.in>
2022-12-05 12:39:09 +0530

    completed google auth flow

--- app/controllers/api/v1/social_media_users_controller.rb
+++ app/controllers/api/v1/social_media_users_controller.rb
@@ -8,10 +8,10 @@ module Api
       def google_oauth2
         url = "https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=#{params["token_id"]}"                  
         response = HTTParty.get(url)
-        user = SocialMediaUser.create_user_for_google(response.parsed_response)
-        token = user.password                      
-        user.save
-        render json: {account: user, token: token }
+        response = SocialMediaUser.create_user_for_google(response.parsed_response)
+        if response[:check] 
+          render json: {success: true, data: response[:data] }
+        end 
       end
     end
   end
--- app/models/social_media_user.rb
+++ app/models/social_media_user.rb
@@ -1,11 +1,10 @@
 class SocialMediaUser < User
   def self.create_user_for_google(data)
     email = data["email"]
-    if User.find_by_email(email).nil?
-      
-      User.create!(email: email, password: 1234)
-    else
-      return true
+    user = User.find_by_email(email)
+    if user.nil?
+      user =  SocialMediaUser.create!(email: email, password: 12345678)
     end
+    return {check: true, data: user} 
   end 
 end
